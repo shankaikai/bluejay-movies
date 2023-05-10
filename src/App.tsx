276e89fbf35fad42103ca0630233d711d9d1b0f3
Tiong Shan Kai <shankaikai@Tiongs-MacBook-Pro.local>
2023-05-10 15:14:44 +0800

    Added routing and placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { MantineProvider, Text } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
+import ErrorPage from "./pages/ErrorPage";
 
-export default function App() {
+export default function App(): JSX.Element {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/movie/:id",
+          element: <MoviePage />,
+        },
+      ],
+    },
+    {
+      path: "/*",
+      element: <ErrorPage />,
+    },
+  ]);
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <RouterProvider router={router} />
     </MantineProvider>
   );
 }
---
+++ src/pages/MoviePage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MoviePage(): JSX.Element {
+  return <div>MoviePage</div>;
+}
