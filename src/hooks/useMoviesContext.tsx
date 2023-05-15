4a45f3a535886dd25452f0170dbd3e3669d7fe2b
Tiong Shan Kai <tiongshankai97@gmail.com>
2023-05-15 15:19:55 +0800

    fix: Removed inconsistent filteredMovies state from context

--- src/hooks/useMoviesContext.tsx
+++ src/hooks/useMoviesContext.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, createContext, useEffect, useState } from "react";
 import useMovieQuery, { Movie } from "./useMovieQuery";
-import filterMovies from "../helpers/filterMovies";
 
 export interface IndexedMovie extends Movie {
   index: number;
@@ -15,16 +14,13 @@ export interface FiltersInterface {
 interface MoviesContextInterface {
   isLoading: boolean;
   indexedMovies: IndexedMovie[];
-  filteredMovies: IndexedMovie[];
   filters: FiltersInterface;
   setFilters?: Dispatch<FiltersInterface>;
-  setFilteredMovies?: Dispatch<IndexedMovie[]>;
 }
 
 export const MoviesContext = createContext<MoviesContextInterface>({
   isLoading: false,
   indexedMovies: [],
-  filteredMovies: [],
   filters: {
     genres: [],
     startYear: 1900,
@@ -39,7 +35,6 @@ interface MoviesProviderProps {
 export function MoviesProvider({ children }: MoviesProviderProps) {
   const { isLoading, data, error } = useMovieQuery();
   const [indexedMovies, setIndexedMovies] = useState<IndexedMovie[]>([]);
-  const [filteredMovies, setFilteredMovies] = useState<IndexedMovie[]>([]);
   const [filters, setFilters] = useState<FiltersInterface>({
     genres: [],
     startYear: 1900,
@@ -62,25 +57,16 @@ export function MoviesProvider({ children }: MoviesProviderProps) {
       });
     }
 
-    indexedMoviesList && setFilteredMovies(indexedMoviesList);
     indexedMoviesList && setIndexedMovies(indexedMoviesList);
   }, [data]);
-
-  useEffect(() => {
-    indexedMovies &&
-      setFilteredMovies &&
-      filterMovies(indexedMovies, setFilteredMovies, filters);
-  }, [filters]);
 
   return (
     <MoviesContext.Provider
       value={{
         isLoading,
         indexedMovies,
-        filteredMovies,
         filters,
         setFilters,
-        setFilteredMovies,
       }}
     >
       {children}
